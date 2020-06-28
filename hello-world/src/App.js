b081bfd3efa161065ad246d17dab28a14a61b31a
harishtm <harishtm77@gmail.com>
2020-06-28 16:32:56 +0530

    named export

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet' // ommit .js extension
+import { Greet } from './components/Greet' // ommit .js extension
 
 function App() {
   return (
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// function Greet(){
+//     return <h1> Welcome to React JS </h1>;
+// }
 
-function Greet(){
-    return <h1> Welcome to React JS </h1>
-}
+// ES6 arrow functions
 
-export default Greet;
+export const Greet = () => <h1> Welcome to React JS </h1> // This is a named export
+
+// export default Greet;
