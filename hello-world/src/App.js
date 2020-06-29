d4b50928b7d63e1412038515250ee226913a21f7
harishtm <harishtm77@gmail.com>
2020-06-29 20:06:39 +0530

    class component with named export

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet' // ommit .js extension
+import { WelcomeMessage } from './components/Welcome'
+
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <WelcomeMessage/>
     </div>
   );
 }
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+
+export class WelcomeMessage extends Component {
+    render() {
+        return <h1> My new class Component </h1>
+    }
+}
