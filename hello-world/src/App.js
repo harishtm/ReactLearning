d2fc4b5068713ea16016fa8a6d9ed12509aa8d4f
harishtm <harishtm77@gmail.com>
2020-06-30 09:36:03 +0530

    Understanding JSX

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/Greet' // ommit .js extension
-import { WelcomeMessage } from './components/Welcome'
+import { Greet } from './components/Greet'; // ommit .js extension
+import { WelcomeMessage } from './components/Welcome';
+import Hello from './components/Hello';
 
 
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
-      <WelcomeMessage/>
+      {/* <Greet></Greet> */}
+      {/* <WelcomeMessage/> */}
+      <Hello/>
     </div>
   );
 }
---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/* 
+// Using JSX Approach 
+const Hello = () => {
+    return (
+        <div>
+            <h1> Understanding JSX </h1>
+        </div>
+    )
+}
+*/
+
+//
+
+// const Hello
+
+export default Hello;
