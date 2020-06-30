c23e0a2bcab34e6ad02817d3495a0670bb76a726
harishtm <harishtm77@gmail.com>
2020-06-30 09:51:31 +0530

    Without JSX approach

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,18 +1,43 @@
 import React from 'react';
 
+
 /* 
-// Using JSX Approach 
+    Using JSX Approach
+*/
+
+/*
 const Hello = () => {
     return (
-        <div>
+        <div className='myId'>
             <h1> Understanding JSX </h1>
         </div>
     )
 }
 */
 
-//
+/*
+    Without using JSX approach
 
-// const Hello
+    createElement() method accepts 3 parametrs
+    1. a string - HTML tag to render
+    2. optional properties
+        - optional parameter is an object of key value pair
+        - which can hold any HTML properties
+    3. children for the HTML element (i.e) childern for the div tag
+*/
+
+const Hello = () => {
+    return (
+        React.createElement(
+            'div',
+            {id: 'mydivId', className: 'myCssClass'},
+            React.createElement(
+                'h1',
+                null,
+                'Understanding JSX - Without JSX')
+            )
+    )
+}
+
 
 export default Hello;
