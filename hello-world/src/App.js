a9d867dafd95f05ac7d70bd73ccf86478013b618
harishtm <harishtm77@gmail.com>
2020-07-03 09:40:45 +0530

    state management in a component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,22 +5,31 @@ import { Greet } from './components/Greet'; // ommit .js extension
 import { WelcomeMessage } from './components/Welcome';
 import Hello from './components/Hello';
 import { GreetMessage, EmployeeGreet } from './components/GreetWithName';
+import Message from './components/Message';
 
 
 function App() {
   return (
     <div className="App">
       {/* <Greet></Greet> */}
+      
       {/* <WelcomeMessage/> */}
+      
       {/* <Hello/> */}
-      <GreetMessage firstname="John" lastname="Smith">
+      
+      {/* <GreetMessage firstname="John" lastname="Smith">
         <p> This is poprs child text </p>
-      </GreetMessage>
-      <GreetMessage firstname="Michel" lastname="Clark">
+      </GreetMessage> */}
+      
+      {/* <GreetMessage firstname="Michel" lastname="Clark">
         <button>Action</button>
-      </GreetMessage>
-      <GreetMessage firstname="Stev" lastname="Smith"/>
-      <EmployeeGreet firstname="Ricky" lastname="Pointing"/>
+      </GreetMessage> */}
+      
+      {/* <GreetMessage firstname="Stev" lastname="Smith"/>
+      <EmployeeGreet firstname="Ricky" lastname="Pointing"/> */}
+
+      <Message/>
+
     </div>
   );
 }
---
+++ hello-world/src/components/Message.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+
+class Message extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: "Welcome to state management in React."
+        }
+    }
+
+    changeMessage() {
+        this.setState({
+            message: 'Thank you for subscribing'
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    { this.state.message }
+                </h1>
+                <button onClick={ () => this.changeMessage() }>
+                    Subscribe
+                </button>
+            </div>
+        )
+    }
+}
+
+
+export default Message;
