a9687380f35ebff5013c66220d6fed2af53e73b2
harishtm <harishtm77@gmail.com>
2020-12-31 22:33:06 +0530

    axios post

--- reacthttp/src/components/PostForm.js
+++ reacthttp/src/components/PostForm.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 
@@ -13,19 +14,52 @@ class PostForm extends Component {
         }
     }
 
+    changeHandler = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = e => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     render() {
+        const {userId, title, body} = this.state;
         return (
             <div>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     <div>
-                        <input type="text" name="userId"/>
+                        <label>userId</label>
+                        <input
+                            type="text"
+                            name="userId"
+                            value={userId}
+                            onChange={this.changeHandler}/>
                     </div>
                     <div>
-                        <input type="text" name="title"/>
+                        <label>Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            value={title}
+                            onChange={this.changeHandler}/>
                     </div>
                     <div>
-                        <input type="text" name="body"/>
+                        <label>Content</label>
+                        <input
+                            type="text"
+                            name="body"
+                            value={body}
+                            onChange={this.changeHandler}/>
                     </div>
+                    <button type="submit">Submit</button>
                 </form>
 
             </div>
