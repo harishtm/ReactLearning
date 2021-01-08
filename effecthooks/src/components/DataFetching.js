a68052eb6bcd3decb8ca53140ad1baa0e0d15159
harishtm <harishtm77@gmail.com>
2021-01-08 10:49:25 +0530

    data fetching with different useEffect scenario

--- effecthooks/src/components/DataFetching.js
+++ effecthooks/src/components/DataFetching.js
@@ -1,27 +1,45 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 function DataFetching() {
 
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({});
+    const [id, setId] = useState(1);
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+    const [error, setError] = useState('')
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(response => {
             console.log(response)
-            setPosts(response.data)
+            setPost(response.data)
+            setError('')
         })
         .catch(error => {
-            console.log(error)
+            console.log(error.response)
+            setError(error.response.status)
+            setPost({})
         })
-    }, [])
+    }, [idFromButtonClick])
 
     return(
         <div>
-            {
+            <input type="text" value={id} onChange={e => setId(e.target.value)}/>
+            <button type="button" onClick={handleClick}>Fetch Post</button>
+            
+            <div>
+                { error ? error : post.title}
+                {/* {post.title} */}
+            </div>
+
+            {/* {
                 posts.map(item => <li key={item.id}>{item.title}</li>)
-            }
+            } */}
         </div>
     )
 }
