8e467980ae90c62e93f583ece3bceda45ebe7ff7
harishtm <harishtm77@gmail.com>
2021-05-04 11:05:12 +0530

    custom hook p2

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
 
 function App() {
   return (
     <div className="App">
       <DocTitleOne/>
+      <DocTitleTwo/>
     </div>
   );
 }
---
+++ custom-hooks/src/components/DocTitleTwo.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+
+function DocTitleTwo() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Count - ${count}`
+    }, [count])
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Count - {count}</button>
+        </div>
+    )
+}
+
+export default DocTitleTwo;
