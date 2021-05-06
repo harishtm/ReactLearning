8d6fcf5c20eb4f6e36b1c41c873a9cd380f70e93
harishtm <harishtm77@gmail.com>
2021-05-06 10:34:59 +0530

    custom hook example 2

---
+++ custom-hooks/src/components/CounterTwo.js
@@ -0,0 +1,35 @@
+import useCounter from '../hooks/useCounter';
+
+
+function CounterTwo() {
+
+/*
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+*/
+
+    const [count, increment, decrement, reset] = useCounter(5, 5);
+
+    return (
+        <div>
+            <h2>Count - {count}</h2>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
+
+
+export default CounterTwo;
---
+++ custom-hooks/src/hooks/useCounter.js
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+
+
+// Custom Hook - for re-usability
+
+function useCounter(initialCount = 0, value = 1) {
+
+    const [count, setCount] = useState(initialCount)
+
+    const increment = () => {
+        setCount(prevCount => prevCount + value)
+    }
+
+    const decrement = () => {
+        setCount(prevCount => prevCount - value)
+    }
+
+    const reset = () => {
+        setCount(initialCount)
+    }
+
+    return [count, increment, decrement, reset]
+
+}
+
+export default useCounter;
