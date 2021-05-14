d41de5bc66a71727b7dc7e501775d37a4f43c00e
harishtm <harishtm77@gmail.com>
2021-05-14 22:00:08 +0530

    redux example 1

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -1,1 +1,26 @@
-console.log("Redux demo")
+const BUY_CAKE = 'BUY_CAKE'
+
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCake: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCake: state.numOfCake - 1
+        }
+
+        default: return state
+    }
+}
