b69e75cb5c285e45d19b21f05b667ba46c7601da
harishtm <harishtm77@gmail.com>
2021-05-16 14:16:16 +0530

    simpel redux Middleware (logger) and applying while createStore

--- REDUX-DEMO/index.js
+++ REDUX-DEMO/index.js
@@ -1,7 +1,12 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
+
 const createStore = redux.createStore
 // To combine multiple reducers into single reducer
 const combineReducers = redux.combineReducers;
+const logger = reduxLogger.createLogger()
+const applyMiddleware = redux.applyMiddleware
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
@@ -61,9 +66,11 @@ const rootReducer = combineReducers({
     cake: cakeReducer,
     iceCream: iceCreamReducer
 })
-const store = createStore(rootReducer)
+
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial state', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
