9c791cce230019173a64470b2009c9d4d797ab11
harishtm <harishtm77@gmail.com>
2021-05-18 08:50:57 +0530

    reducer and store

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Provider } from 'react-redux'
+import { store } from './redux/cake/store'
 import CakeContainer from './components/CakeContainer';
+
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer/>
+      </div>
+    </Provider>
+    
   );
 }
 
