4ffd74af16d96411fb816ac9a5168afba1f2d425
harishtm <harishtm77@gmail.com>
2021-06-05 19:14:31 +0530

    useSelector and useDispatch hook in redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Provider } from 'react-redux'
 import { store } from '../src/redux/cake/store'
 import CakeContainer from '../src/components/CakeContainer';
+import HooksCakeContainer from '../src/components/HooksCakeContainer'
 
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer/>
         <CakeContainer/>
       </div>
     </Provider>
