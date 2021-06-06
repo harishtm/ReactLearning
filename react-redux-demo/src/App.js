6cce5e1cc4ab7ba5366b4a77f73a4ceaa604d805
harishtm <harishtm77@gmail.com>
2021-06-06 12:42:12 +0530

    mapState with props additonal paramter

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -5,12 +5,15 @@ import CakeContainer from '../src/components/CakeContainer';
 import HooksCakeContainer from '../src/components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
 import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer/>
         <HooksCakeContainer/>
         <CakeContainer/>
         <IceCreamContainer/>
