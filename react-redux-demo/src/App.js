7a616bcd5d5ce8e52fc2458151ea62d2521f280d
harishtm <harishtm77@gmail.com>
2021-06-06 17:00:39 +0530

    async action in redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import { Provider } from 'react-redux'
-import { store } from '../src/redux/cake/store'
-import CakeContainer from '../src/components/CakeContainer';
-import HooksCakeContainer from '../src/components/HooksCakeContainer'
-import IceCreamContainer from './components/IceCreamContainer'
-import NewCakeContainer from './components/NewCakeContainer';
-import ItemContainer from './components/ItemContainer';
+import { store } from '../src/redux/store'
+// import CakeContainer from '../src/components/CakeContainer';
+// import HooksCakeContainer from '../src/components/HooksCakeContainer'
+// import IceCreamContainer from './components/IceCreamContainer'
+// import NewCakeContainer from './components/NewCakeContainer';
+// import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake/>
+        <UserContainer/>
+        {/* <ItemContainer cake/>
         <ItemContainer/>
         <HooksCakeContainer/>
         <CakeContainer/>
         <IceCreamContainer/>
-        <NewCakeContainer/>
+        <NewCakeContainer/> */}
       </div>
     </Provider>
     
--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 // import cakeReducer from './cakeReducer'
-import rootReducer from '../rootReducer'
+import rootReducer from './rootReducer'
+import thunk from 'redux-thunk'
 
 
 export const store = createStore(rootReducer, composeWithDevTools(
-                            applyMiddleware(logger)
+                            applyMiddleware(logger, thunk)
                         ))
